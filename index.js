f837ea3474fecc40a042b7cc1aca736105aa6560
Leonard <leonard.muhati@student.moringaschool.com>
2024-06-27 13:26:07 +0300

    My arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
+function multiply(num1,num2) {
+    num1*num2;
+}
+multiply(31,2);
+var result=multiply(31,2);
+function random(){
+    Math.floor(Math.random() * 11);
+}
+function mod(num3,num4){
+    num3%num4
+}
+  (14%10)
+  var result=mod(14%10)
+  function max(){
+    Math.max(17,18,19,20)
+  }
 // Write your code here
