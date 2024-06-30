9e44eb349fe438a94045f4a46b5330ba6011c41a
Leonard <leonard.muhati@student.moringaschool.com>
2024-06-30 12:40:28 +0300

    arithmetics lab 2

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-function multiply(num1,num2) {
-    num1*num2;
-}
-multiply(31,2);
-var result=multiply(31,2);
-function random(){
-    Math.floor(Math.random() * 11);
-}
-function mod(num3,num4){
-    num3%num4
-}
-  (14%10)
-  var result=mod(14%10)
-  function max(){
-    Math.max(17,18,19,20)
-  }
+const num1=31
+const num2=2
+const multiply=num1*num2
+const random=Math.round(Math.random()*100)+1;
+const num3=24
+const num4=10
+const mod=(num3%num4);
+const max=Math.max(16,17,18,19,20);
 // Write your code here
